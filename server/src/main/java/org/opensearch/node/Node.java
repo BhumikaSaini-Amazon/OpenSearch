e3219983b578bc3927ac8c78f24251b712ea8371
Bhumika Saini <sabhumik@amazon.com>
2023-09-02 19:50:25 +0530

    Incorporate PR review feedback - 1
    Signed-off-by: Bhumika Saini <sabhumik@amazon.com>

--- server/src/main/java/org/opensearch/node/Node.java
+++ server/src/main/java/org/opensearch/node/Node.java
@@ -742,7 +742,6 @@ public class Node implements Closeable {
             );
 
             remoteStoreStatsTrackerFactory = new RemoteStoreStatsTrackerFactory(clusterService, settings);
-
             final IndicesService indicesService = new IndicesService(
                 settings,
                 pluginsService,
